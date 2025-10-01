2397cb09b786e41f7399ff3b01711693e7b8e524
magic-clicks <225111685+magic-clicks@users.noreply.github.com>
2025-10-01 18:03:43 -0500

    checked property

--- In-Progress Testing/index.js
+++ In-Progress Testing/index.js
@@ -157,4 +157,36 @@ ageSubmit.onclick = function (){
     }
 }
 
-// 
+// Next script for checked property
+
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn= document.getElementById("visaBtn");
+const masterCardBtn= document.getElementById("masterCardBtn");
+const payPalBtn= document.getElementById("payPalBtn");
+const mySubmitBtn= document.getElementById("mySubmitBtn");
+const subResult= document.getElementById("subResult");
+const paymentResult= document.getElementById("paymentResult");
+
+mySubmitBtn.onclick = function(){
+
+    if(myCheckBox.checked){
+        subResult.textContent = "You are subscribed!";
+    }
+    else{
+        subResult.textContent = "You are NOT subscribed!";
+    }
+
+   if(visaBtn.checked){
+    paymentResult.textContent = "You are paying with Visa";
+   }
+   else if(masterCardBtn.checked){
+    paymentResult.textContent = "You are paying with MasterCard";
+   }
+   else if(payPalBtn.checked){
+    paymentResult.textContent = "You are paying with PayPal";
+   }
+   else{
+    paymentResult.textContent = "You have not selected a payment type";
+   }
+  
+}
