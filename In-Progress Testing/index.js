4f062e5f2923221c4134a7f9fde9b1f4eeb5114d
magic-clicks <225111685+magic-clicks@users.noreply.github.com>
2025-10-01 16:03:42 -0500

    69

--- In-Progress Testing/index.js
+++ In-Progress Testing/index.js
@@ -137,7 +137,7 @@ ageSubmit.onclick = function (){
     age = Number(age);
     
     if(age == 0){
-        resultElement.textContent = "You can't enter. You were just born";
+        resultElement.textContent = "You can't enter. You were just born!";
     }
     else if(age <0){
         resultElement.textContent = "Your age can't be below 0";
@@ -145,9 +145,13 @@ ageSubmit.onclick = function (){
     else if(age >=100){
         resultElement.textContent = "You are too old to enter this site";
     }
+    else if(age == 69){
+        resultElement.textContent = "Lmao";
+    }
     else if(age >= 18){   
     resultElement.textContent = "You are old enough to enter this site";
     }
+    
     else{
         resultElement.textContent = "You must be 18+ to enter this site";
     }
