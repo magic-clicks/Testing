bf2755af2db5941c132a288ff5338459d9911762
magic-clicks <225111685+magic-clicks@users.noreply.github.com>
2025-11-01 13:24:34 -0500

    Code clean up

--- In-Progress Testing/index.js
+++ In-Progress Testing/index.js
@@ -610,32 +610,32 @@ let running = true;
 
 guessingGame.onclick = function(){
 
-while(running == true){
+    while(running == true){
 
-    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
-    guess = Number(guess);
-    
-    if(isNaN(guess)){
-        window.alert("Please enter a valid number");
-    }
-    else if(guess < minNum || guess > maxNum){
-        window.alert("Please enter a valid number");
-    }
-    else{
-        attempts++
-        if(guess < answer){
-            window.alert("Too low! Try again!");
-
+        guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+        guess = Number(guess);
+        
+        if(isNaN(guess)){
+            window.alert("Please enter a valid number");
         }
-        else if(guess > answer){
-            window.alert("Too high! Try again!");
-
+        else if(guess < minNum || guess > maxNum){
+            window.alert("Please enter a valid number");
         }
         else{
-            window.alert(`Correct! The answer was ${answer}. It took you ${attempts} attempts`);
-            running = false;
+            attempts++
+            if(guess < answer){
+                window.alert("Too low! Try again!");
+
+            }
+            else if(guess > answer){
+                window.alert("Too high! Try again!");
+
+            }
+            else{
+                window.alert(`Correct! The answer was ${answer}. It took you ${attempts} attempts`);
+                running = false;
+            }
         }
     }
-    
 }
-}
+
