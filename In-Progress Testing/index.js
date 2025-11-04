749d2d18d5f963299517e723f5211aa5ac901883
magic-clicks <225111685+magic-clicks@users.noreply.github.com>
2025-11-03 20:51:55 -0600

    Created tip calculator function

--- In-Progress Testing/index.js
+++ In-Progress Testing/index.js
@@ -817,3 +817,46 @@ function sugarConversion(){
     }
 
 }
+
+// more practice making a bit more complicated - creating a tip calculator 
+
+
+const tip = document.getElementById("tip");
+const tenPercent = document.getElementById("tenPercent");
+const fifteenPercent = document.getElementById("fifteenPercent");
+const twentyPercent = document.getElementById("twentyPercent");
+const twentyFivePercent = document.getElementById("twentyFivePercent");
+const percentResult = document.getElementById("percentResult");
+let tipResult;
+let tipOriginal;
+
+function tipCalculator(){
+
+    if(tenPercent.checked){
+        tipResult = Number(tip.value);
+        tipOriginal = Number(tip.value);
+        tipResult = tipResult *.10 + tipOriginal;
+        percentResult.textContent = "Total = $" + tipResult;
+    }
+    else if(fifteenPercent.checked){
+        tipResult = Number(tip.value);
+        tipOriginal = Number(tip.value);
+        tipResult = tipResult *.15 + tipOriginal;
+        percentResult.textContent = "Total = $" + tipResult;
+    }
+    else if(twentyPercent.checked){
+        tipResult = Number(tip.value);
+        tipOriginal = Number(tip.value);
+        tipResult = tipResult *.20 + tipOriginal;
+        percentResult.textContent = "Total = $" + tipResult;
+    }
+    else if(twentyFivePercent.checked){
+        tipResult = Number(tip.value);
+        tipOriginal = Number(tip.value);
+        tipResult = tipResult *.25 + tipOriginal;
+        percentResult.textContent = "Total = $" + tipResult;
+    }
+    else{
+        percentResult.textContent = "Please select a percentage";
+    }
+}
