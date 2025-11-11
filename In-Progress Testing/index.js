f0a6781baa59aa310599e1641694e821a107c07c
magic-clicks <225111685+magic-clicks@users.noreply.github.com>
2025-11-11 12:46:57 -0600

    2d Arrays

--- In-Progress Testing/index.js
+++ In-Progress Testing/index.js
@@ -911,3 +911,33 @@ console.log(index);
 fruits.sort();
 console.log(fruits);
 
+// 2D arrays = multi-dimensional array that stores a matrix 
+// of data in rows and columns
+// useful as games, spreadsheets, or representing images
+
+// x, o, x
+// o, x, o
+// x, o, x
+
+const matrix = [[1, 2, 3],
+                [4, 5, 6,],
+                [7, 8, 9]];
+
+matrix[0][0] = `X`;
+matrix[0][1] = `O`;
+matrix[0][2] = `X`;
+
+matrix[1][0] = `O`;
+matrix[1][1] = `X`;
+matrix[1][2] = `O`;
+
+matrix[2][0] = `X`;
+matrix[2][1] = `O`;
+matrix[2][2] = `X`;
+
+
+for(let row of matrix){
+    const rowString = row.join(` `);
+        console.log(row);
+}
+
