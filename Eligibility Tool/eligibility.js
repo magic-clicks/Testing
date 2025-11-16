12b21a96f8e86125d04a7c38ddbe04f61487ec98
magic-clicks <225111685+magic-clicks@users.noreply.github.com>
2025-11-15 22:25:24 -0600

    Testing first line of function: Success

--- Eligibility Tool/eligibility.js
+++ Eligibility Tool/eligibility.js
@@ -1,12 +1,19 @@
 console.log("test");
 
+const yesResident = document.getElementById("yesResident");
+const noResident = document.getElementById("noResident");
 
 
-
-
+const toolResult = document.getElementById("toolResult");
 
 
 function eligibility(){
 
+    if(yesResident.checked){
+        toolResult.textContent = "You are within the eligible population!";
+    }
+    else{
+        toolResult.textContent = "You are NOT within the eligible population";
+    }
 
 }
