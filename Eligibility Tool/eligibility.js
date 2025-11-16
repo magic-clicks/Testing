bf1699199b5a34a7abed4aa0ccdf13948d7c3512
magic-clicks <225111685+magic-clicks@users.noreply.github.com>
2025-11-15 22:38:27 -0600

    Completed const's in JS

--- Eligibility Tool/eligibility.js
+++ Eligibility Tool/eligibility.js
@@ -2,6 +2,19 @@ console.log("test");
 
 const yesResident = document.getElementById("yesResident");
 const noResident = document.getElementById("noResident");
+const yesCit = document.getElementById("yesCit");
+const noCit = document.getElementById("noCit");
+const yesCTM = document.getElementById("yesCTM");
+const noCTM = document.getElementById("noCTM");
+const yesMinor = document.getElementById("yesMinor");
+const notMinor = document.getElementById("notMinor");
+const yesPregnant = document.getElementById("yesPregnant");
+const notPregnant = document.getElementById("notPregnant");
+const yesBCC = document.getElementById("yesBCC");
+const notBCC = document.getElementById("notBCC");
+const yesFoster = document.getElementById("yesFoster");
+const notFoster = document.getElementById("notFoster");
+
 const toolResult = document.getElementById("toolResult");
 
 
