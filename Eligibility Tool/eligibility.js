f6744c0f5c84219f44ce5079f12ac8e795def29d
magic-clicks <225111685+magic-clicks@users.noreply.github.com>
2025-11-15 22:33:26 -0600

    Completed form Id's and Names

--- Eligibility Tool/eligibility.js
+++ Eligibility Tool/eligibility.js
@@ -2,8 +2,6 @@ console.log("test");
 
 const yesResident = document.getElementById("yesResident");
 const noResident = document.getElementById("noResident");
-
-
 const toolResult = document.getElementById("toolResult");
 
 
@@ -17,3 +15,7 @@ function eligibility(){
     }
 
 }
+
+// writing thoughts down - what if I write up each checked property as its own function
+// and then I write the submit button to run each funtion in succession as long as they keep passing
+// that might be the most legit way to do it
