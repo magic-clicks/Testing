c3f0efaa9f665652bfa66cf18ef39a75148b2917
magic-clicks <225111685+magic-clicks@users.noreply.github.com>
2025-11-24 10:21:09 -0600

    Random Password Generator

---
+++ Password Generator/script.js
@@ -0,0 +1,43 @@
+// random password generator
+
+
+function generatePassword(length, includeLowerCase, includeUpperCase, includeNumbers, includeSymbols){
+
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWYXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_-=+"
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowerCase ? lowercaseChars : "";
+    allowedChars += includeUpperCase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    if(length <= 0){
+        return `(password must be at least 1)`;
+    }
+    if(allowedChars === 0){
+        return `(at least 1 set of characters need to be selected)`;
+    }
+
+    for(let i =0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+    
+    return password;
+}
+
+
+const passwordLength = 12;
+const includeLowerCase = true;
+const includeUpperCase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, includeLowerCase, includeUpperCase, includeNumbers, includeSymbols);
+
+console.log(`Generated password ${password}`);
