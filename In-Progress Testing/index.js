41b435c47f8674441a040188a7ad1fe68ac15260
magic-clicks <225111685+magic-clicks@users.noreply.github.com>
2025-11-26 19:59:31 -0600

    Callbacks

--- In-Progress Testing/index.js
+++ In-Progress Testing/index.js
@@ -1023,3 +1023,42 @@ const realName = combineString("Mr", "Spongebob", "Squarepants", "III");
 
 console.log(realName);
 
+// callback = a function that is passed as an argument to another function.
+
+// used to handel asynchronus operations:
+// 1. Reading a file
+// 2. Newtwork requests
+// 3. Interacting with databses
+
+// "Hey when you're done, call this next"
+
+hello(leave); //type the other function into the arguement 
+
+function hello(callback){
+    console.log("Hello");
+    callback();
+}
+
+function wait(){
+    console.log("Wait!");
+}
+
+function leave(){
+    console.log("Leave!");
+}
+
+function goodbye(){
+    console.log("Goodbye!");
+}
+
+// next example
+
+//sum(displayConsole, 1, 2);
+
+//function sum(callback, x, y){
+//    let result = x + y;
+//    callback(result);
+//}
+//function displayConsole(result){
+//    console.log(result);
+//}
