cc75af0467eed1a899c6a320ad23f7f66697b99e
magic-clicks <225111685+magic-clicks@users.noreply.github.com>
2025-11-29 15:38:42 -0600

    Destructuring

---
+++ Destructuring/script.js
@@ -0,0 +1,6 @@
+// destructuring = extract values from arrays and objects, 
+// then assign them to variables in a convienent way
+// [] = to perform array destructing
+// {} = to perform object destructing 
+// 5 examples
+
