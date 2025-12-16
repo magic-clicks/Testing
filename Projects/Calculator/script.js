762f1629265192340a342fe0d94f505ca2bf4645
magic-clicks <225111685+magic-clicks@users.noreply.github.com>
2025-12-15 22:18:45 -0600

    Finished Calculator

--- Projects/Calculator/script.js
+++ Projects/Calculator/script.js
@@ -1,2 +1,26 @@
 // Calculator program
 console.log("test");
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+
+    display.value += input;
+
+}
+
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculate(){
+
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "error";
+}    
+
+}
